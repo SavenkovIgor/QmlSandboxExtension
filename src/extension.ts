aaefd4512ec7a14fc0e1a402e7827f3a46556b05
SavenkovIgor <savenkovigor.dev@gmail.com>
2023-06-25 16:53:37 +0300

    Replace tabs with spaces

--- src/extension.ts
+++ src/extension.ts
@@ -2,41 +2,41 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('QmlSandboxExtension.openQmlSandbox', () => {
+    let disposable = vscode.commands.registerCommand('QmlSandboxExtension.openQmlSandbox', () => {
 
-		const qmlEngineFolder = vscode.Uri.joinPath(context.extensionUri, 'wasmQmlEngine');
+        const qmlEngineFolder = vscode.Uri.joinPath(context.extensionUri, 'wasmQmlEngine');
 
-		const panel = vscode.window.createWebviewPanel(
-			'QmlSandbox',
-			'QML Sandbox',
-			vscode.ViewColumn.Two,
-			{
-				enableScripts: true,
-				localResourceRoots: [qmlEngineFolder]
-			} 
-		);
+        const panel = vscode.window.createWebviewPanel(
+            'QmlSandbox',
+            'QML Sandbox',
+            vscode.ViewColumn.Two,
+            {
+                enableScripts: true,
+                localResourceRoots: [qmlEngineFolder]
+            } 
+        );
 
-		const indexHtmlPath = vscode.Uri.joinPath(qmlEngineFolder, 'index.html');
+        const indexHtmlPath = vscode.Uri.joinPath(qmlEngineFolder, 'index.html');
 
-		vscode.workspace.fs.readFile(indexHtmlPath).then((fileData) => {
-			let htmlContent = fileData.toString();
-			
-			// Disk paths
-			const qtLoaderJs = vscode.Uri.joinPath(qmlEngineFolder, 'qtloader.js');
-			const qtLogoSvg  = vscode.Uri.joinPath(qmlEngineFolder, 'qtlogo.svg');
+        vscode.workspace.fs.readFile(indexHtmlPath).then((fileData) => {
+            let htmlContent = fileData.toString();
+            
+            // Disk paths
+            const qtLoaderJs = vscode.Uri.joinPath(qmlEngineFolder, 'qtloader.js');
+            const qtLogoSvg  = vscode.Uri.joinPath(qmlEngineFolder, 'qtlogo.svg');
 
-			// Replace paths for startup js script and logo svg
-			htmlContent = htmlContent.replace('qtloader.js', panel.webview.asWebviewUri(qtLoaderJs).toString());
-			htmlContent = htmlContent.replace('qtlogo.svg', panel.webview.asWebviewUri(qtLogoSvg).toString());
+            // Replace paths for startup js script and logo svg
+            htmlContent = htmlContent.replace('qtloader.js', panel.webview.asWebviewUri(qtLoaderJs).toString());
+            htmlContent = htmlContent.replace('qtlogo.svg', panel.webview.asWebviewUri(qtLogoSvg).toString());
 
-			// Add proper path prefix for loading QtWasmTemplate.js and QtWasmTemplate.wasm
-			htmlContent = htmlContent.replace('vscode_extension_uri_qml_engine_path', panel.webview.asWebviewUri(qmlEngineFolder).toString());
+            // Add proper path prefix for loading QtWasmTemplate.js and QtWasmTemplate.wasm
+            htmlContent = htmlContent.replace('vscode_extension_uri_qml_engine_path', panel.webview.asWebviewUri(qmlEngineFolder).toString());
 
-			panel.webview.html = htmlContent;
-		});
-	});
+            panel.webview.html = htmlContent;
+        });
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
