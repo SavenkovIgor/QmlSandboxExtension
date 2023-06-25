5df53d3b302432b487e1cb02bbc34e8db6cc4b8e
SavenkovIgor <savenkovigor.dev@gmail.com>
2023-06-25 19:31:53 +0300

    Move panel creation to function

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,24 @@
 import * as vscode from 'vscode';
 
+let disposables: vscode.Disposable[] = [];
+
 export function activate(context: vscode.ExtensionContext) {
 
     let disposable = vscode.commands.registerCommand('QmlSandboxExtension.openQmlSandbox', () => {
 
         const qmlEngineFolder = vscode.Uri.joinPath(context.extensionUri, 'wasmQmlEngine');
 
-        const panel = vscode.window.createWebviewPanel(
-            'QmlSandbox',
-            'QML Sandbox',
-            vscode.ViewColumn.Two,
-            {
-                enableScripts: true,
-                localResourceRoots: [qmlEngineFolder]
-            } 
-        );
 
         const indexHtmlPath = vscode.Uri.joinPath(qmlEngineFolder, 'index.html');
 
         vscode.workspace.fs.readFile(indexHtmlPath).then((fileData) => {
             let htmlContent = fileData.toString();
-            
+
             // Disk paths
             const qtLoaderJs = vscode.Uri.joinPath(qmlEngineFolder, 'qtloader.js');
             const qtLogoSvg  = vscode.Uri.joinPath(qmlEngineFolder, 'qtlogo.svg');
+
+            const panel = createQmlPanel([qmlEngineFolder]);
 
             // Replace paths for startup js script and logo svg
             htmlContent = htmlContent.replace('qtloader.js', panel.webview.asWebviewUri(qtLoaderJs).toString());
@@ -39,5 +34,19 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
+function createQmlPanel(roots: vscode.Uri[]) {
+    return vscode.window.createWebviewPanel(
+        'QmlSandbox',
+        'QML Sandbox',
+        vscode.ViewColumn.Two,
+        {
+            enableScripts: true,
+            localResourceRoots: roots
+        }
+    );
+}
+
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    disposables.forEach((disposable) => disposable.dispose());
+}
