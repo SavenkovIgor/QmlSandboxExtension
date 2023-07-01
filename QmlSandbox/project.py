3ab48c9a490a2802b5495b158ad48025d2bb7f59
SavenkovIgor <savenkovigor.dev@gmail.com>
2023-07-01 16:56:31 +0300

    add small project.py fixes

--- QmlSandbox/project.py
+++ QmlSandbox/project.py
@@ -4,10 +4,11 @@ import subprocess, os
 import argparse
 from pathlib import Path
 
+qt_version = '6.5.1'
+emsdk_version = '3.1.25'
 
 def repository_root() -> Path:
     return Path(__file__).parent
-
 
 def system_call(command: str, env: dict = None):
     if subprocess.call(command, shell=True, executable='/bin/bash', env=env) != 0:
@@ -19,6 +20,11 @@ def delete_if_exist(path: Path):
         print(f'Delete {path}')
         system_call(f'rm -rf {path}')
 
+def prepare_env():
+    env = os.environ.copy()
+    env['QT_ROOT'] = str(repository_root() / 'QmlSandbox/Qt')
+    env['QT_VERSION'] = '6.5.1'
+    return env
 
 class Project:
     def root_dir(self) -> Path:
@@ -33,16 +39,27 @@ class Project:
         print('Install aqtinstall tool')
         system_call('pip install aqtinstall')
 
-        print('Installing Qt 6.5.1 with aqtinstall tool')
-        system_call('aqt install-qt linux desktop 6.5.1 gcc_64 --outputdir ./Qt')
-        system_call('aqt install-qt linux desktop 6.5.1 wasm_32 --outputdir ./Qt')
+        if not (self.root_dir() / 'Qt').exists():
+            print(f'Installing Qt {qt_version} with aqtinstall tool')
+            system_call(f'aqt install-qt linux desktop {qt_version} gcc_64 --outputdir ./Qt')
+            system_call(f'aqt install-qt linux desktop {qt_version} wasm_singlethread --outputdir ./Qt')
+        else:
+            print('Qt already installed at ./Qt')
+
+    def install_emsdk(self):
+        if not (self.root_dir() / 'emsdk').exists():
+            print('Install emsdk')
+            system_call('git clone https://github.com/emscripten-core/emsdk.git')
+            os.chdir(self.root_dir() / 'emsdk')
+            system_call(f'./emsdk install {emsdk_version}')
+            system_call(f'./emsdk activate {emsdk_version}')
+            system_call('source ./emsdk_env.sh')
+        else:
+            print('emsdk already installed at ./emsdk')
 
     def build(self):
         print(f'---BUILD {self.name()}---')
-
-        env = os.environ.copy()
-        env['QT_ROOT'] = self.root_dir() / 'Qt'
-        env['QT_VERSION'] = '6.5.1'
+        env = prepare_env()
 
         # Configure cmake
         system_call('cmake --preset=wasm_release -Wno-dev', env=env)
@@ -56,16 +73,25 @@ class Project:
 
     def remove_qt_install(self):
         print(f'---Remove installed Qt---')
-        delete_if_exist(self.root_dir() / '6.5.1')
+        delete_if_exist(self.root_dir() / 'Qt')
+
+    def remove_emsdk(self):
+        print(f'---Remove emsdk---')
+        delete_if_exist(self.root_dir() / 'emsdk')
+
+    def remove_venv(self):
+        print(f'---Remove virtual environment---')
+        delete_if_exist(self.root_dir() / 'QtBuildEnv')
 
 
 def main():
     parser = argparse.ArgumentParser(description='Project build script')
 
-    parser.add_argument('--install',   action='store_true', help='Install Qt in project folder')
-    parser.add_argument('--build',     action='store_true', help='Build project')
-    parser.add_argument('--clear',     action='store_true', help='Clear project')
-    parser.add_argument('--clear-all', action='store_true', help='Clear all')
+    parser.add_argument('--create-venv', action='store_true', help='Create virtual environment')
+    parser.add_argument('--install',     action='store_true', help='Install Qt in project folder')
+    parser.add_argument('--build',       action='store_true', help='Build project')
+    parser.add_argument('--clear',       action='store_true', help='Clear project')
+    parser.add_argument('--clear-all',   action='store_true', help='Clear all')
 
     args = parser.parse_args()
 
@@ -77,9 +103,16 @@ def main():
     if args.clear_all:
         app.remove_build()
         app.remove_qt_install()
+        app.remove_emsdk()
+        app.remove_venv()
+
+    if args.create_venv:
+        system_call('python -m venv QtBuildEnv')
+        print('To activate virtual environment: source QtBuildEnv/bin/activate')
 
     if args.install:
         app.install_qt()
+        app.install_emsdk()
 
     if args.build:
         app.build()
