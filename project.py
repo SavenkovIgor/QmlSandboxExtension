c48838c77dccde0464d4a480595c22cf74df4fb1
SavenkovIgor <savenkovigor.dev@gmail.com>
2023-08-09 01:16:21 +0300

    Move project.py

--- project.py
+++ project.py
@@ -7,81 +7,85 @@ from pathlib import Path
 qt_version = '6.5.1'
 emsdk_version = '3.1.25'
 
-def repository_root() -> Path:
+def root() -> Path:
     return Path(__file__).parent
 
-def system_call(command: str, env: dict = None):
+def run(command: str, env: dict = None):
     if subprocess.call(command, shell=True, executable='/bin/bash', env=env) != 0:
         exit(1)
-
 
 def delete_if_exist(path: Path):
     if path.exists():
         print(f'Delete {path}')
-        system_call(f'rm -rf {path}')
+        run(f'rm -rf {path}')
 
 def prepare_env():
     env = os.environ.copy()
-    env['QT_ROOT'] = str(repository_root() / 'QmlSandbox/Qt')
+    env['QT_ROOT'] = str(root() / 'QmlSandbox/Qt')
     env['QT_VERSION'] = '6.5.1'
     return env
 
 class Project:
-    def root_dir(self) -> Path:
-        return Path(__file__).parent
+    def __init__(self):
+        self.root             = root()
+        self.name             = self.root.name
+        self.venv_root        = self.root / 'QtBuildEnv'
+        self.qml_sandbox_root = self.root / 'QmlSandbox'
 
-    def name(self) -> str:
-        return self.root_dir().name
+        self.qt_root    = self.qml_sandbox_root / 'Qt'
+        self.emsdk_root = self.qml_sandbox_root / 'emsdk'
 
     def install_qt(self):
-        print(f'---INSTALL {self.name()}---')
+        print(f'---INSTALL {self.name}---')
 
         print('Install aqtinstall tool')
-        system_call('pip install aqtinstall')
+        run('pip install aqtinstall')
 
-        if not (self.root_dir() / 'Qt').exists():
+        if not (self.qt_root).exists():
             print(f'Installing Qt {qt_version} with aqtinstall tool')
-            system_call(f'aqt install-qt linux desktop {qt_version} gcc_64 --outputdir ./Qt')
-            system_call(f'aqt install-qt linux desktop {qt_version} wasm_singlethread --outputdir ./Qt')
+            run(f'aqt install-qt linux desktop {qt_version} gcc_64 --outputdir {self.qt_root}')
+            run(f'aqt install-qt linux desktop {qt_version} wasm_singlethread --outputdir {self.qt_root}')
         else:
-            print('Qt already installed at ./Qt')
+            print(f'Qt already installed at {self.qt_root}')
 
     def install_emsdk(self):
-        if not (self.root_dir() / 'emsdk').exists():
+        if not (self.emsdk_root).exists():
+            os.chdir(self.qml_sandbox_root)
             print('Install emsdk')
-            system_call('git clone https://github.com/emscripten-core/emsdk.git')
-            os.chdir(self.root_dir() / 'emsdk')
-            system_call(f'./emsdk install {emsdk_version}')
-            system_call(f'./emsdk activate {emsdk_version}')
-            system_call('source ./emsdk_env.sh')
+            run('git clone https://github.com/emscripten-core/emsdk.git')
+            os.chdir(self.emsdk_root)
+            run(f'./emsdk install {emsdk_version}')
+            run(f'./emsdk activate {emsdk_version}')
+            run('source ./emsdk_env.sh')
         else:
             print('emsdk already installed at ./emsdk')
 
-    def build(self):
-        print(f'---BUILD {self.name()}---')
+    def build_qml(self):
+        print(f'---BUILD {self.name}---')
         env = prepare_env()
+        os.chdir(self.qml_sandbox_root)
 
         # Configure cmake
-        system_call('cmake --preset=wasm_release -Wno-dev', env=env)
+        run('cmake --preset=wasm_release -Wno-dev', env=env)
 
         # Build
-        system_call('cmake --build --preset=wasm_release', env=env)
+        run('cmake --build --preset=wasm_release', env=env)
 
     def remove_build(self):
         print(f'---Remove build folder---')
-        delete_if_exist(self.root_dir() / 'build')
+        delete_if_exist(self.qml_sandbox_root / 'build')
 
     def remove_qt_install(self):
         print(f'---Remove installed Qt---')
-        delete_if_exist(self.root_dir() / 'Qt')
+        delete_if_exist(self.qt_root)
 
     def remove_emsdk(self):
         print(f'---Remove emsdk---')
-        delete_if_exist(self.root_dir() / 'emsdk')
+        delete_if_exist(self.emsdk_root)
 
     def remove_venv(self):
         print(f'---Remove virtual environment---')
-        delete_if_exist(self.root_dir() / 'QtBuildEnv')
+        delete_if_exist(self.venv_root)
 
 
 def main():
@@ -89,7 +93,7 @@ def main():
 
     parser.add_argument('--create-venv', action='store_true', help='Create virtual environment')
     parser.add_argument('--install',     action='store_true', help='Install Qt in project folder')
-    parser.add_argument('--build',       action='store_true', help='Build project')
+    parser.add_argument('--build-qml',   action='store_true', help='Build project')
     parser.add_argument('--clear',       action='store_true', help='Clear project')
     parser.add_argument('--clear-all',   action='store_true', help='Clear all')
 
@@ -107,15 +111,15 @@ def main():
         app.remove_venv()
 
     if args.create_venv:
-        system_call('python -m venv QtBuildEnv')
+        run('python -m venv QtBuildEnv')
         print('To activate virtual environment: source QtBuildEnv/bin/activate')
 
     if args.install:
         app.install_qt()
         app.install_emsdk()
 
-    if args.build:
-        app.build()
+    if args.build_qml:
+        app.build_qml()
 
 
 if __name__ == '__main__':
