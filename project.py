065f5dbf5b7373b9f58cf252009ed0d5c1b5b09d
Savenkov Igor <savenkovigor.dev@gmail.com>
2023-08-10 12:34:24 +0300

    21 fix GitHub actions and automatic delivery to vscode marketplace (#24)
    * Add deliver qml step with copy of delivery files
    
    * Add qtlogo.svg
    
    * Cleanup in vscodeignore
    
    * add .github to vscodeignore
    
    * Draft for action
    
    * Add npm install
    
    * Move Qt to repo root for all build cases
    
    * add emsdk cache to vscodeignore
    
    * Remove waste info from internal cmake
    
    * Fix gitignore for qtlogo.svg
    
    * Remove deploy folder
    
    * Cleanup in project.py
    
    * Further cleanup of project.py
    
    * Move Qt to lib subfolder
    
    * Qt path fix for ghAction
    
    * Move emsdk to lib folder
    
    * Move python environment to env/python, remove gcc install step from aqtinstall
    
    * Add version checking

--- project.py
+++ project.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
-import subprocess, os
-import argparse
+import subprocess, os, shutil, argparse, json
 from pathlib import Path
 
 qt_version = '6.5.1'
 emsdk_version = '3.1.25'
-
-def root() -> Path:
-    return Path(__file__).parent
 
 def run(command: str, env: dict = None):
     if subprocess.call(command, shell=True, executable='/bin/bash', env=env) != 0:
@@ -19,57 +15,62 @@ def delete_if_exist(path: Path):
         print(f'Delete {path}')
         run(f'rm -rf {path}')
 
-def prepare_env():
-    env = os.environ.copy()
-    env['QT_ROOT'] = str(root() / 'QmlSandbox/Qt')
-    env['QT_VERSION'] = '6.5.1'
-    return env
-
 class Project:
     def __init__(self):
-        self.root             = root()
+        self.root             = Path(__file__).parent
         self.name             = self.root.name
-        self.venv_root        = self.root / 'QtBuildEnv'
+        self.venv_root        = self.root / 'env/python'
         self.qml_sandbox_root = self.root / 'QmlSandbox'
+        self.wasm_engine_root = self.root / 'wasmQmlEngine'
 
-        self.qt_root    = self.qml_sandbox_root / 'Qt'
-        self.emsdk_root = self.qml_sandbox_root / 'emsdk'
+        self.lib        = self.root / 'lib'
+        self.qt_root    = self.lib / 'Qt'
+        self.emsdk_root = self.lib / 'emsdk'
+
+    def prepare_env(self):
+        env = os.environ.copy()
+        env['QT_ROOT'] = str(self.qt_root)
+        env['QT_VERSION'] = qt_version
+        return env
 
     def install_qt(self):
         print(f'---INSTALL {self.name}---')
 
-        print('Install aqtinstall tool')
-        run('pip install aqtinstall')
-
         if not (self.qt_root).exists():
             print(f'Installing Qt {qt_version} with aqtinstall tool')
-            run(f'aqt install-qt linux desktop {qt_version} gcc_64 --outputdir {self.qt_root}')
-            run(f'aqt install-qt linux desktop {qt_version} wasm_singlethread --outputdir {self.qt_root}')
+            py_env_prefix = f'{self.venv_root}/bin/python -m '
+            output_dir = f'--outputdir {self.qt_root}'
+            modules = '--modules qtimageformats qt5compat qtshadertools'
+            archives = '--archives qttranslations qttools qtsvg qtdeclarative qtbase icu'
+            run(f'{py_env_prefix} aqt install-qt linux desktop {qt_version} wasm_singlethread {output_dir} {modules} {archives} --autodesktop')
         else:
             print(f'Qt already installed at {self.qt_root}')
 
     def install_emsdk(self):
         if not (self.emsdk_root).exists():
-            os.chdir(self.qml_sandbox_root)
             print('Install emsdk')
+            os.chdir(self.emsdk_root.parent)
             run('git clone https://github.com/emscripten-core/emsdk.git')
             os.chdir(self.emsdk_root)
             run(f'./emsdk install {emsdk_version}')
             run(f'./emsdk activate {emsdk_version}')
-            run('source ./emsdk_env.sh')
         else:
-            print('emsdk already installed at ./emsdk')
+            print(f'emsdk already installed at {self.emsdk_root}')
 
     def build_qml(self):
-        print(f'---BUILD {self.name}---')
-        env = prepare_env()
+        print(f'---Build of qml project---')
+        env = self.prepare_env()
         os.chdir(self.qml_sandbox_root)
+        run('cmake --preset=wasm_release -Wno-dev', env=env)  # Configure cmake
+        run('cmake --build --preset=wasm_release', env=env)  # Build
 
-        # Configure cmake
-        run('cmake --preset=wasm_release -Wno-dev', env=env)
-
-        # Build
-        run('cmake --build --preset=wasm_release', env=env)
+    def deliver_qml(self):
+        print(f'---Deliver qml build to repo root---')
+        build_path = self.qml_sandbox_root / 'build/wasm_release'
+        shutil.copy(build_path / 'qtloader.js', self.wasm_engine_root)
+        shutil.copy(build_path / 'qtlogo.svg', self.wasm_engine_root)
+        shutil.copy(build_path / 'QtWasmTemplate.js', self.wasm_engine_root)
+        shutil.copy(build_path / 'QtWasmTemplate.wasm', self.wasm_engine_root)
 
     def remove_build(self):
         print(f'---Remove build folder---')
@@ -87,17 +88,32 @@ class Project:
         print(f'---Remove virtual environment---')
         delete_if_exist(self.venv_root)
 
+    def last_version_tag(self):
+        cmd = 'git describe --tags --match="v[0-9\.]*" --abbrev=0'
+        return subprocess.check_output(cmd, shell=True).decode().strip()
+
+    def package_version(self):
+        with open(self.root / 'package.json') as f:
+            return json.load(f)['version']
+
 
 def main():
     parser = argparse.ArgumentParser(description='Project build script')
 
-    parser.add_argument('--create-venv', action='store_true', help='Create virtual environment')
-    parser.add_argument('--install',     action='store_true', help='Install Qt in project folder')
-    parser.add_argument('--build-qml',   action='store_true', help='Build project')
-    parser.add_argument('--clear',       action='store_true', help='Clear project')
-    parser.add_argument('--clear-all',   action='store_true', help='Clear all')
+    parser.add_argument('--create-venv',   action='store_true', help='Create virtual environment')
+    parser.add_argument('--install',       action='store_true', help='Install Qt in project folder')
+    parser.add_argument('--build-qml',     action='store_true', help='Build project')
+    parser.add_argument('--deliver-qml',   action='store_true', help='Copy delivery folder from qml build to wasmQmlEngine folder')
+    parser.add_argument('--check-version', action='store_true', help='Check if version from tag is equal to version in package.json')
+    parser.add_argument('--clear',         action='store_true', help='Clear project')
+    parser.add_argument('--clear-all',     action='store_true', help='Clear all')
 
     args = parser.parse_args()
+
+    # Print help if no arguments
+    if not any(vars(args).values()):
+        parser.print_help()
+        exit(1)
 
     app = Project()
 
@@ -111,8 +127,17 @@ def main():
         app.remove_venv()
 
     if args.create_venv:
-        run('python -m venv QtBuildEnv')
-        print('To activate virtual environment: source QtBuildEnv/bin/activate')
+        print(f'---Create virtual environment---')
+        if not (app.venv_root).exists():
+            run(f'python3 -m venv {app.venv_root}')
+            run(f'{app.venv_root}/bin/pip install --upgrade pip')
+
+            print('Install aqtinstall tool')
+            run(f'{app.venv_root}/bin/pip install aqtinstall')
+            print('To activate virtual environment run:')
+            print(f'source {app.venv_root}/bin/activate')
+        else:
+            print(f'Venv already exists at {app.venv_root}')
 
     if args.install:
         app.install_qt()
@@ -121,6 +146,18 @@ def main():
     if args.build_qml:
         app.build_qml()
 
+    if args.deliver_qml:
+        app.deliver_qml()
+
+    if args.check_version:
+        tag_version = app.last_version_tag()
+        tag_version = tag_version[1:]  # Remove first letter 'v' from version
+        package_version = app.package_version()
+        if tag_version != package_version:
+            print(f'Version in package.json ({package_version}) is not equal to version from tag ({tag_version})')
+            exit(1)
+        else:
+            print(f'Version in package.json ({package_version}) is equal to version from tag ({tag_version})')
 
 if __name__ == '__main__':
     main()
