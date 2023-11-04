de9125934c0c61460e4bec3472a60fbba9c4d589
SavenkovIgor <savenkovigor.dev@gmail.com>
2023-11-04 16:36:04 +0200

    Refactor message handlers to use switch statement

--- wasmQmlEngine/windowHandlers.js
+++ wasmQmlEngine/windowHandlers.js
@@ -1,16 +1,14 @@
 function installVscodeToWasmMessageHandlers(instance) {
     window.addEventListener('message', (event) => {
-        const handlers = {
-            update: ({ text }) => { instance?.newCodeHandler(text); },
-            screenshot: () => { instance?.screenshotHandler(); }
-        };
-
-        const handler = handlers[event.data.type];
-
-        if (handler) {
-            handler(event.data);
-        } else {
-            console.error(`Unknown message type: ${event.data.type}`);
+        switch (event.data.type) {
+            case 'update':
+                instance?.newCodeHandler(event.data.text);
+                break;
+            case 'screenshot':
+                instance?.screenshotHandler();
+                break;
+            default:
+                console.error(`Unknown message type: ${event.data.type}`);
         }
     });
 }
